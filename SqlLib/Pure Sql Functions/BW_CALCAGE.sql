3e41d30f414e87e04b84de7a2eb0f844248a4992
BlueWombat <mikkel@mikkelp.com>
2014-07-25 23:40:00 +0200

    Adding oure sql functions

---
+++ SqlLib/Pure Sql Functions/BW_CALCAGE.sql
@@ -0,0 +1,32 @@
+﻿-- ===============================================================================
+-- Author:		BlueWombat (http://bluewombat.com)
+-- Create date: 25-07-2014
+-- Description:	A nice little helper method to calculate age based on birth date
+-- ===============================================================================
+CREATE FUNCTION [dbo].[BW_CALCAGE]
+(
+	@birthDate datetime
+)
+RETURNS int
+AS
+BEGIN
+	declare
+		@today datetime = GETDATE()
+	declare 
+		@yearsOld int = DATEDIFF(year, @birthDate, @today)
+
+	select
+		@yearsOld = case
+						when @today >= DATETIMEFROMPARTS(YEAR(@today), MONTH(@birthDate), DAY(@birthDate), 0, 0, 0, 0) then
+							@yearsOld
+						else
+							@yearsOld - 1
+						end
+	return
+		case
+			when @yearsOld < 0 then
+				0
+			else
+				@yearsOld
+			end
+END
---
+++ SqlLib/Pure Sql Functions/BW_STRINGSPLIT.sql
@@ -0,0 +1,52 @@
+﻿-- ======================================================================================
+-- Author:		BlueWombat (http://bluewombat.com)
+-- Create date: 25-07-2014
+-- Description:	Provides a simple split implementation and thereby pseudo array support
+-- ======================================================================================
+CREATE FUNCTION [dbo].[BW_STRINGSPLIT]
+(	
+	@stringToSplit varchar(max),
+	@splitOn varchar(max)
+)
+RETURNS
+@result table
+(
+	value varchar(max)
+)
+AS
+BEGIN
+	declare
+		@splitOnLen int = len(@splitOn),
+		@remainingString varchar(max) = @stringToSplit
+
+	while len(@remainingstring) > 0 and CHARINDEX(@splitOn, @remainingString) > 0
+		begin
+			declare
+				@splitOnIndexStart int = CHARINDEX(@splitOn, @remainingString)
+			declare
+				@splitOnIndexEnd int = @splitOnIndexStart + @splitOnLen
+			declare
+				@remainingStringIndexEnd int = len(@remainingString) - @splitOnIndexEnd + 1
+		
+			insert into @result
+			(
+				value
+			)
+			select
+				SUBSTRING(@remainingString, 1, @splitOnIndexStart - 1)
+
+			select
+				@remainingString = SUBSTRING(@remainingString, @splitOnIndexEnd, @remainingStringIndexEnd)
+		end
+
+	insert into @result
+	(
+		value
+	)
+	values
+	(
+		@remainingString
+	)
+
+	return
+END
